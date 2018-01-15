6c208a05f3099382e6980b5a02441f008c7c0778
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-01-15 08:20:27 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+    	fs.readFile('index.html', function(err, html) {
+        response.write(html);
+        response.end();
+    });
+
+
+  
+    } else {
+    		response.setHeader("Content-Type", "image/jpeg; charset=utf-8");
+    		fs.readFile('.cat.jpg', function(err, data) {
+        	response.statusCode = 404;
+            response.write(data);
+    		
+        	response.end();
+    		});     
+    }
+});
+
+server.listen(8080);
